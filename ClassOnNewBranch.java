412919c295a80cfc452a0edfbe1510625c421d47
Pengcheng Liu <331815690@qq.com>
2022-07-14 09:40:07 +0800

    update on new branch: branch1

---
+++ ClassOnNewBranch.java
@@ -0,0 +1,12 @@
+/**
+ * @author Pengcheng
+ * @date 2022/7/14
+ */
+public class ClassOnNewBranch
+{
+    public static void main(String[] args)
+    {
+        System.out.println("this is a new branch.");
+        System.out.println("I will merge it a few minutes later.");
+    }
+}
